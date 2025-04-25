0ce46926865792299665a1ccea2c47d3a6f9c534
brownrice <brownrice0916@gmail.com>
2025-04-25 15:57:44 +0900

    build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
 // 이 부분이 중요합니다
-export const GET = handler
-export const POST = handler
+export const GET = handler;
+export const POST = handler;
